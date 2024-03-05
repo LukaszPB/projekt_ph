7aa971a1462f23a8ff65ca68703cab80bbea967b
LukaszPB <lukasz.kiersnowski.110752@student.pb.edu.pl>
2024-03-05 20:33:33 +0100

    Initial commit

---
+++ src/main/java/com/example/projekt_ph/ProjektPhApplication.java
@@ -0,0 +1,13 @@
+package com.example.projekt_ph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektPhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektPhApplication.class, args);
+	}
+
+}
